daca724eedbb78bca02b06ff836d12023d7dbe79
Peter <46304666+peterjorphan@users.noreply.github.com>
2019-06-02 20:23:24 -0500

    added sattelite layer

--- static/js/logic.js
+++ static/js/logic.js
@@ -67,9 +67,17 @@ function createMap(earthquakes) {
     accessToken: API_KEY
   });
 
+  var satellite = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.satellite",
+    accessToken: API_KEY
+  });
+
   // Define a baseMaps object to hold our base layers
   var baseMaps = {
     "Street Map": streetmap,
+    "Sattelite": satellite,
     "Dark Map": darkmap
   };
 
